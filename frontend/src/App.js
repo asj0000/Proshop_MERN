87ab484f68dc19ae62ac2b5f8dfc9333c5314905
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2024-01-30 11:21:22 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Outlet } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { Container } from "react-bootstrap"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+       <Outlet/>
+      </Container>
+    </main>
+    <Footer/>
+    </>
   )
 }
 
---
+++ frontend/src/products.js
@@ -0,0 +1,85 @@
+const products = [
+  
+  
+  {
+     _id: '1',
+     name: 'Airpods Wireless Bluetooth headphones',
+     image: '/images/airpods.jpg',
+     description: 'Bluetooth technology lets you connect',
+     brand: 'Apple',
+     category: 'Electronics',
+     price: 89.99,
+     countInStock: 0,
+     rating: 4.5,
+     numReview: 12
+    
+  },
+  {
+    _id: '2',
+    name: 'Airpods Wireless Bluetooth headphones',
+    image: '/images/alexa.jpg',
+    description: 'Bluetooth technology lets you connect',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 0.5,
+    numReview: 12
+   
+ },
+ {
+  _id: '3',
+  name: 'Airpods Wireless Bluetooth headphones',
+  image: '/images/camera.jpg',
+  description: 'Bluetooth technology lets you connect',
+  brand: 'Apple',
+  category: 'Electronics',
+  price: 89.99,
+  countInStock: 10,
+  rating: 1,
+  numReview: 12
+ 
+},
+{
+  _id: '4',
+  name: 'Airpods Wireless Bluetooth headphones',
+  image: '/images/mouse.jpg',
+  description: 'Bluetooth technology lets you connect',
+  brand: 'Apple',
+  category: 'Electronics',
+  price: 89.99,
+  countInStock: 10,
+  rating: 3.5,
+  numReview: 12
+ 
+},
+{
+  _id: '5',
+  name: 'Airpods Wireless Bluetooth headphones',
+  image: '/images/phone.jpg',
+  description: 'Bluetooth technology lets you connect',
+  brand: 'Apple',
+  category: 'Electronics',
+  price: 89.99,
+  countInStock: 10,
+  rating: 2,
+  numReview: 12
+ 
+},
+{
+  _id: '6',
+  name: 'Airpods Wireless Bluetooth headphones',
+  image: '/images/playstation.jpg',
+  description: 'Bluetooth technology lets you connect',
+  brand: 'Apple',
+  category: 'Electronics',
+  price: 89.99,
+  countInStock: 10,
+  rating: 0,
+  numReview: 12
+ 
+}
+
+]
+
+export default products
